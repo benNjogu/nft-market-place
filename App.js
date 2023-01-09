173a7511af3eccf1cc0961753996b04ceac07769
benNjogu <kamandemachafi@gmail.com>
2023-01-09 15:08:54 +0300

    dependency bug fixed

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Again!</Text>
+      <Text>Hello Again! We good</Text>
     </View>
   );
 }
